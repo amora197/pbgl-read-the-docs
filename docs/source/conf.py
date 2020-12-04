5e12d22a8eeae19a1e01f3a9b56470dab4ebc172
Anibal Morales <amora197@fiu.edu>
2020-12-04 15:07:11 +0100

    First run creating the sphinx tutorial

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Anibal Morales'
 author = 'Anibal Morales'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2'
+release = ''
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,26 +30,78 @@ release = '1.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = ['sphinx.ext.autodoc',
+   'sphinx.ext.doctest',
+   'sphinx.ext.intersphinx',
+   'sphinx.ext.todo',
+   'sphinx.ext.coverage',
+   'sphinx.ext.mathjax',
+   'sphinx.ext.ifconfig',
+   'sphinx.ext.viewcode',
+   'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+source_suffix = '.rst'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
 
+import sphinx_rtd_theme
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_add_permalinks = ''
+master_doc = 'index'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
+
+
+# -- Options for LaTeX output ---------------------------------------------
+
+latex_engine = 'pdflatex'
+latex_theme = 'howto'
+latex_toplevel_sectioning = 'section'
+
+
+# Configuration of Title Page
+latex_maketitle = r'''
+        \pagenumbering{Roman} %%% to avoid page 1 conflict with actual page
+        \begin{titlepage}
+            \vspace*{10mm} %%% * is used to give space from top, Title
+            \flushright\textbf{\Huge {Creating a ReadtheDocs Documentation v1.0}}
+            \vspace{0mm} %%% Sub-Title
+            \textbf{\Large {A Step-by-Step Guide}}
+            \vspace{50mm} % Author name
+            \textbf{\Large {Anibal E. Morales}}
+            \vspace{10mm} % Organization Name
+            \textbf{\Large {Plant Breeding and Genetics Laboratory}}
+            \vspace{0mm} % Division/Department
+            \textbf{\Large {FAO/IAEA Joint Division}}
+            \vspace{0mm} % City, Country
+            \textbf{\Large {Seibersdorf, Austria}}
+            \vspace{10mm} % Creation Date
+            \normalsize Created: October, 2020
+            \vspace*{0mm} % Last updated Date
+            \normalsize  Last updated: 4 December 2020
+            %% \vfill adds at the bottom a note or caution
+            \vfill
+            \small\flushleft {{\textbf {Please note:}} \textit {This is an important note at the bottom of the title page.}}
+        \end{titlepage}
+        \pagenumbering{arabic}
+        \newcommand{\sectionbreak}{\clearpage}
+'''
+latex_elements = {
+   'releasename': 'Version 1.2',
+   'maketitle': latex_maketitle,
+}
